9b6e7f70fb3123282ff14e5906ce9ca9b60cd768
kim14420 <101626094+kim14420@users.noreply.github.com>
2022-06-13 22:08:29 +0900

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -1,17 +1,37 @@
 package exam8;
 
+import java.util.Scanner;
+
 public class AccountTest {
 
 	public static void main(String[] args) {
 		
-		Account account = new Account("441-0290-1203", 50000, 7.3);
+		Scanner in = new Scanner(System.in);
 		
-		System.out.println(account);
 		
-		account.deposit(2000);	
+		String accountinformation = "";
+	System.out.print("계좌정보를 입력하세요: ");
+		accountinformation = in.next();
 		
-		System.out.println(account);
-		System.out.println("이자: " + account	.Calculateinterest());
+		
+		int balance = 0;
+	System.out.print("현재잔액을 입력하시오: ");
+		balance = in.nextInt();
+		System.out.println();
+		
+		in.close();
+		
+		double interest = 7.3;
+	
+		
+		Account accountsetting = new Account(accountinformation, balance, interest);
+		
+	System.out.println(accountsetting);
+		
+		accountsetting.deposit(20000);	
+		
+	System.out.println(accountsetting);
+	System.out.println("이자: " + accountsetting.Calculateinterest() * 0.1);
 		
 	
 	}
